511c29e1dee364289edadf712e4d1ee41bd6eafd
王俊超 <shining-glory@qq.com>
2017-08-04 08:33:58 +0800

    commit

--- SpringCloud构建微服务架构（一）服务注册与发现/compute-service/src/main/java/com/didispace/web/ComputeController.java
+++ SpringCloud构建微服务架构（一）服务注册与发现/compute-service/src/main/java/com/didispace/web/ComputeController.java
@@ -4,10 +4,7 @@ import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ComputeController {
@@ -17,12 +14,20 @@ public class ComputeController {
     @Autowired
     private DiscoveryClient client;
 
+    @Deprecated
     @RequestMapping(value = "/add", method = RequestMethod.GET)
     public Integer add(@RequestParam Integer a, @RequestParam Integer b) {
         ServiceInstance instance = client.getLocalServiceInstance();
         Integer r = a + b;
         logger.info("/add, host:" + instance.getHost() + ", service_id:" + instance.getServiceId() + ", result:" + r);
         return r;
+    }
+
+    @GetMapping("/dc")
+    public String dc() {
+        String services = "Services: " + client.getServices();
+        System.out.println(services);
+        return services;
     }
 
 }
