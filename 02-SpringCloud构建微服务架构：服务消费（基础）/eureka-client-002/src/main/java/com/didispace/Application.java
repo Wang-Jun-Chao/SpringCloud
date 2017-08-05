49f14a137a392da0400fe99b991cf21dc7add476
王俊超 <shining-glory@qq.com>
2017-08-05 08:14:56 +0800

    commit

---
+++ 02-SpringCloud构建微服务架构：服务消费（基础）/eureka-client-002/src/main/java/com/didispace/Application.java
@@ -0,0 +1,18 @@
+package com.didispace;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * Author: 王俊超
+ * Date: 2017-08-05 07:08
+ * All Rights Reserved !!!
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(Application.class).web(true).run(args);
+    }
+}
